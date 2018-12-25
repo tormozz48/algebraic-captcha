ef414cf7ea297722af59a8953ccb140879403630
tormozz48 <tormozz48@gmail.com>
2018-12-25 12:22:55 +0300

    test(formula-builder): Add unit-tests for formula-builder module

---
+++ test/formula-builder.test.ts
@@ -0,0 +1,73 @@
+import {expect} from 'chai';
+import {Config} from '../src/config';
+import {FormulaBuilder} from '../src/formula-builder';
+
+describe('src/formula-builder', () => {
+    function generateFormula_(params = {}) {
+        const config = new Config(params);
+        const formulaBuilder = new FormulaBuilder(config);
+
+        return formulaBuilder.generateFormula();
+    }
+
+    it('should generate formula for base configuration with default parameters', () => {
+        const result = generateFormula_({});
+
+        expect(result.answer).to.be.a('number');
+        expect(result.formula).to.be.instanceof(Array);
+
+        expect(result.formula).to.be.lengthOf(5);
+    });
+
+    it('should have valid answer for created equation', () => {
+        const result = generateFormula_({});
+        const answer = new Function(`return ${result.formula.slice(0, 3).join(' ')}`)();
+
+        expect(answer).to.equal(result.answer);
+    });
+
+    it('should use given amount of operators', () => {
+        const result = generateFormula_({operandAmount: 2});
+
+        expect(result.formula).to.be.instanceof(Array);
+        expect(result.formula).to.be.lengthOf(7);
+    });
+
+    it('should append "= ?" suffix to formula ending', () => {
+        const result = generateFormula_({});
+
+        const l = result.formula.length;
+        expect(result.formula[l - 2]).to.equal('=');
+        expect(result.formula[l - 1]).to.equal('?');
+    });
+
+    it('should limit used operands from bottom', () => {
+        for(let i = 0; i < 50; i++) {
+            const result = generateFormula_({minValue: 5});
+
+            expect(Number(result.formula[0])).to.be.above(4);
+            expect(Number(result.formula[2])).to.be.above(4);
+        }
+    });
+
+    it('should limit used operands from top', () => {
+        for(let i = 0; i < 50; i++) {
+            const result = generateFormula_({maxValue: 7});
+
+            expect(Number(result.formula[0])).to.be.below(8);
+            expect(Number(result.formula[2])).to.be.below(8);
+        }
+    });
+
+    it('should limit used operands from top and bottom', () => {
+        for(let i = 0; i < 50; i++) {
+            const result = generateFormula_({minValue: 3, maxValue: 7});
+
+            expect(Number(result.formula[0])).to.be.below(8);
+            expect(Number(result.formula[2])).to.be.below(8);
+
+            expect(Number(result.formula[2])).to.be.above(2);
+            expect(Number(result.formula[2])).to.be.above(2);
+        }
+    });
+});
